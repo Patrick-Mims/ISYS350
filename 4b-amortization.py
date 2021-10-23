330ec50eda92d0444af5a27ab3c6a1395051f6db
Patrick Mims <patrickkevinmims@gmail.com>
2021-10-22 20:26:18 -0700

    Minor change

--- 4b-amortization.py
+++ 4b-amortization.py
@@ -31,27 +31,17 @@ print (col_6.rjust(17, ' '), col_7.rjust(17, ' '), col_8.rjust(23, ' '), col_9.r
 print("\t____________________________________________________________________________________________________")
 print ("\n")
 
-count = 0
-
-#monthly_payment = (loan_amount * (interest_rate / months)) / (1 - (1 + interest_rate / 12) **-months) 
+count = 1
 
 monthly_payment = (loan_amount * (interest_rate / months)) / (1 - (1 + interest_rate / 12) **-months) 
-while count < months:
+
+while count <= months:
     interest_amount = (loan_amount * (interest_rate / months))
     principal_amount = monthly_payment - interest_amount
     balance_remaining = (loan_amount - principal_amount)
+
     print ("\t{0}{1:25.2f}{2:30.2f}{3:30.2f}{4:30.2f}".format(count, round(monthly_payment, 2), round(interest_amount, 2), round(principal_amount, 2), round(balance_remaining, 2)))
+
     count = count + 1
+
     loan_amount = balance_remaining 
-
-    # Interest
-    # The amount of interest column is calculated by multiplying the 
-    # previous months remaining balance by annual_rate / 12
-
-    # Principal
-    # The amount of principal is calculated by subtracting the amount
-    # of interest from the monthly payment.
-
-    # Remaining Balance
-    # The remaining balance is calculated by subtracting the amount of
-    # principal from the previous month's remaining balance. 
