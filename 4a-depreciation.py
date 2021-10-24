9fc5ae8dfad59e58daeaf8305d3abd9ed3504af6
Patrick Mims <patrickkevinmims@gmail.com>
2021-10-23 19:14:04 -0700

    latest minor variable name changes

--- 4a-depreciation.py
+++ 4a-depreciation.py
@@ -2,26 +2,25 @@
 
 count = 1
 
-property_value = int(input("Enter property value: "))
-property_life = int(input("Enter property life: "))
+prop_value= int(input("Enter property value: "))
+prop_life = int(input("Enter property life: "))
 
 print("\n")
 print("\tDouble Declining Depreciation Table\n")
-print("\tProperty value: ", property_value)
-print("\tProperty life: ", property_life,"\n")
+print("\tProperty value: ", prop_value)
+print("\tProperty life: ", prop_life,"\n")
 
 year = "Year"
-year_beginning = "Value at"
-depriciationDuringYear = "Depreciation"
-tp = "Total depreciation"
+begin_year= "Value at"
+during_year = "Depreciation"
+total_depreciation = "Total depreciation"
 
 empty = " "
 b_year = "Begin Year" 
 d_year = "During Year" 
 e_year = "To End Year" 
 
-#print("\tYear\tDepreciation During the Year\tTotal depreciation to year end.")
-print (year.rjust(12,' '), year_beginning.rjust(17, ' '), depriciationDuringYear.rjust(24, ' '), tp.rjust(25, ' '))
+print (year.rjust(12,' '), begin_year.rjust(17, ' '), during_year.rjust(24, ' '), total_depreciation.rjust(25, ' '))
 print (empty.rjust(12,' '), b_year.rjust(19, ' '), d_year.rjust(21, ' '), e_year.rjust(19, ' '))
 print("\t__________________________________________________________________________________________________________\n")
 
@@ -29,9 +28,9 @@ count = 0
 depreciation = 0
 total_depreciation = 0
 
-while property_life > count:
+while prop_life > count:
     count = count + 1
-    depreciation = property_value * 2 / property_life
+    depreciation = prop_value * 2 / prop_life
     total_depreciation = total_depreciation + depreciation
-    print("\t{0}{1:20.2f}{2:20.2f}{3:20.2f}".format(count, round(property_value, 2), round(depreciation, 2), round(total_depreciation, 2)));
-    property_value = property_value - depreciation
+    print("\t{0}{1:20.2f}{2:20.2f}{3:20.2f}".format(count, round(prop_value, 2), round(depreciation, 2), round(total_depreciation, 2)))
+    prop_value = prop_value - depreciation
