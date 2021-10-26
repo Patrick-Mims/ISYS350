c6ed2f3e65a2cbba2226354a700d420b628608d8
Patrick Mims <patrickkevinmims@gmail.com>
2021-10-26 10:17:28 -0700

    project init

--- 1a-assignment.py
+++ 1a-assignment.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python
 
+loanAmount = int(input("Enter loan amount: "))
+interestRate = float(input("Enter rate (4.5% entered as 0.045): "))
+loanTerm = int(input("Enter term (in years): "))
+mortgage = 2123.45;
 
+print("With a",loanAmount,"at a",interestRate,"interest rate for",loanTerm,"years, the montly payment is:",mortgage)
