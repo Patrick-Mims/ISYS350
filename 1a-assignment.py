e4489595a901b81432df682cd8e7e82fd25cbb5d
Patrick Mims <patrickkevinmims@gmail.com>
2021-10-26 10:23:57 -0700

    latest

--- 1a-assignment.py
+++ 1a-assignment.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 
-loanAmount = int(input("Enter loan amount: "))
-interestRate = float(input("Enter rate (4.5% entered as 0.045): "))
-loanTerm = int(input("Enter term (in years): "))
+loan = int(input("Enter loan amount: "))
+rate = float(input("Enter rate (4.5% entered as 0.045): "))
+term = int(input("Enter term (in years): "))
+
 mortgage = 2123.45;
 
-print("With a",loanAmount,"at a",interestRate,"interest rate for",loanTerm,"years, the montly payment is:",mortgage)
+print("A loan of",loan,"at an interest rate of",rate,"for",term,"years is:",mortgage,"per month.")
+
+# A loan of $600,000 at an interest rate of 0.045% for 30 years is $2345.67 per month.
