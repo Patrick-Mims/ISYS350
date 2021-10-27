0d9d87872bb23e5dff30fb5518d5a138fd9e8682
Patrick Mims <patrickkevinmims@gmail.com>
2021-10-27 15:12:08 -0700

    latest outputing ....

--- 1a-assignment.py
+++ 1a-assignment.py
@@ -4,10 +4,18 @@ loan = int(input("Enter loan amount: "))
 rate = float(input("Enter rate (4.5% entered as 0.045): "))
 term = int(input("Enter term (in years): "))
 
-#mortgage = 2123.45;
+##monthly_payment = (amt_loan * (interest_rate / months)) / (1 - (1 + interest_rate / 12) **-months)
+monthly_payment = ((loan * (rate / term)) / (1 - (1 + rate / 12) **-term*12))
 
-monthly_payment = loan * (rate/term)
+#monthly_payment *= monthly_payment * term
+currency = "${:,.2f}".format(monthly_payment)
+loan = "${:,.2f}".format(loan)
 
-print("A loan of",loan,"at an interest rate of",rate,"for",term,"years is:",monthly_payment,"per month.")
+#rate = rate * 100
+rate = "{:.2%}".format(rate)
+
+#loan * ((rate * ((1 + rate) ** term)) / ((1 + rate) ** term - 1))
+
+print("A loan of",loan,"at an interest rate of",rate,"% for",term,"years is:",currency, "per month.")
 
 # A loan of $600,000 at an interest rate of 0.045% for 30 years is $2345.67 per month.
