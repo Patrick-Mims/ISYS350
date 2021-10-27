ea09c07f7471babf3f9b4fb08ef212de46724bd7
Patrick Mims <patrickkevinmims@gmail.com>
2021-10-27 16:27:32 -0700

    latest

--- 3b-electric_co.py
+++ 3b-electric_co.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python
 
 count = 0
+amount = 0
 
-while count < 6:
+while count < 2:
     used_kwh = int(input("Enter Kilowatt-Hours used: "))
     print(used_kwh)
     if used_kwh <= 50:
         print(".35")
+        amount = used_kwh * .035
     elif (used_kwh > 50 and used_kwh <= 150):
+        amount += (used_kwh * .035)
         print(".45")
     elif (used_kwh > 150 and used_kwh <= 450):
+        amount += (used_kwh * .065)
         print(".65")
     elif (used_kwh > 450 and used_kwh <= 2000):
+        amount += (used_kwh * .080)
         print(".80")
-    #elif (used_kwh > 2000):
     else:
         print("Kilowatt Hours cannot be more than 2000!");
 
     count = count + 1
 
-
+print("Kilowatt-Hours used: ",used_kwh,", charge is: ",amount)
