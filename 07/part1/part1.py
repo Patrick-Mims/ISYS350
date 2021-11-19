1ff9127efd1b72da58b60af0bde828161b6c9e8f
patrick <Patrick Mims>
2021-11-18 18:32:12 -0800

    latest

--- 07/part1/part1.py
+++ 07/part1/part1.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python
+# Name: Patrick Mims
+# Assignment: 7 (Part 1)
+# Date: November 18, 2021
+# Professor: David Chao
+# #
 
+# necessary variables
 check = 1
 count = 0
 
+# while loop for the phrase to search
 while check != 0:
     phrase = input("Enter a search phrase: ") 
     answer = input("Would you like to enter another phrase? (Y/N) ")
 
+    # I check for the answer being Y/y or N/n
     if((answer == 'N') or (answer == 'n')):
+        # if the answer is n/N I then set the 
+        # 'check' variable to 0 and exit the loop
         check = 0
     elif((answer == 'Y') or (answer == 'y')):
         check = 1
@@ -16,6 +26,7 @@ while check != 0:
 
 print("\n")
 
+# while loop for the paragraph to search
 while check != 1:
     paragraph = input("Enter a paragraph for analysis: ")
     _answer = input("Would you like to enter another paragraph? (Y/N) ")
@@ -34,50 +45,3 @@ for item in paragraph_list:
         count += 1
 
 print(phrase,"appears",count,"times")
-        
-
-#for item in phraseList:
-#
-#        countWord = 0
-#
-#    if searchWord.upper() in paragraph.upper():
-#        countWord = paragraph.upper().count(searchWord.upper())
-#        print(str(countWord))
-#
-#for word in paragraph:
-#
-#print("Your paragraph: ", paragraph.count(phrase[i]))
-#
-#print(paragraph.count())
-#
-#if phrase in paragraph:
-#   print("Yes, the phrase is in the paragraph")
-#   found = True 
-#else:
-#   print("No it's not found")
-#   found = False 
-#
-#
-#if found:
-#   phrase_count = phrase.count('S')
-#   print(str(phrase_count))
-
-#   Part 1: 
-#
-#   Text Frequency Analysis 
-#
-#   Create a Python program that asks users to enter any number of texts
-#   they are interested in analyzing and stores the texts in a list. 
-#
-#   A 'text' can be a word such as "like", or a phrase such as "I like it." Then, the 
-#   program will ask users to enter paragraphs (you may copy from a news article), 
-#   and for each text in the list, display either "Not Found" message if the text 
-#   does not exist in the paragraphs; otherwise display the number of times the text 
-#   is found. 
-#
-#   Hint: 
-#   1. See examples on Slide # 18 to 24 of the Python Collections PowerPoint. 
-#   2. You may use the "in" command and count method as explained on slide 18 and 19.
-#
-#   Do a case-insensitive search by applying string's lower() or upper() function to the search
-#   texts and the search paragraphs. You may arrange code as below:
