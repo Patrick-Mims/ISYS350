0ff591c442133d5eba41466aaf98084fc232948f
Patrick Mims <patrickkevinmims@gmail.com>
2021-11-24 15:46:47 -0800

    Added Comments

--- 07/part2/js/app.js
+++ 07/part2/js/app.js
@@ -2,33 +2,36 @@
  * Date: 11.18.21
  * Assignment: 7
  * Professor: David Chao
- * Purpose: this script simply calculates the monthly value 1347.13
+ * Purpose: Simple Mortgage Calculator
  * */
 (function() {
   var discount = document.getElementById("discount");
-  var interestRate = document.getElementById("interestRate");
+  var rate = document.getElementById("interestRate");
   var loan = document.getElementById("loanAmount");
   var payment = document.getElementById("monthlyPayment");
-  var submit = document.getElementById("submit");
+  var button = document.getElementById("submit");
   var term = document.getElementsByName('term');
 
-  submit.addEventListener("click", function() {
-    var vetDiscount = 0.0;
+  button.addEventListener("click", function() {
+    var vetDiscount = 0.0, termInMonths = 120;
 
+    /* Calculate Term */
     for(i = 0; i < term.length; i++) {
-      if(term[i].checked)
-      {
-        var termInMonths = (term[i].value * 12);
+      if(term[i].checked) {
+        termInMonths = (term[i].value * 12);
       }
     }
 
+    /* Discount? */
     if(discount.checked == true) {
       vetDiscount = (50 * 0.01);
     }
 
-    var convertedInterestRate = ((interestRate.value - vetDiscount) * 0.01);
-    var monthlyPayment = (loan.value * (convertedInterestRate / 12) / (1 - (1 + (convertedInterestRate / 12))**-termInMonths));
+    /* Calculate Monthly Payment */
+    const convertedInterestRate = ((rate.value - vetDiscount) * 0.01);
+    const monthlyPayment = (loan.value * (convertedInterestRate / 12) / (1 - (1 + (convertedInterestRate / 12))**-termInMonths));
 
+    /* Output */
     payment.value = "$" + monthlyPayment.toFixed(2);
   });
 })();
